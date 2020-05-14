0dfa01eceab3fa825332fe7add14c88dd8b72a42
unyuu <u@nyuu.ru>
2020-05-14 13:51:30 +0500

    Синусоида o_O

--- test5.cc
+++ test5.cc
@@ -16,15 +16,22 @@ int main()
         "    x   | sin(x) | cos(x) | tan(x) " << std::endl <<
         "--------+--------+--------+--------" << std::endl;
     for (double x = start; x <= end; x += step) {
+
+        int ww = int( sin(x) * 30 + 32 );
+
         std::cout <<
             std::fixed <<
             std::setw(8) << std::setprecision(3) << x << '|' <<
             std::setprecision(4) <<
             std::setw(8) << sin(x) << '|' <<
             std::setw(8) << cos(x) << '|' <<
-            std::setw(8) << tan(x) << std::endl;
-
+            std::setw(8) << tan(x) << '|' <<
+            std::setw(ww) << '*' << std::endl;
     }
+
+    std::cout <<
+        std::setw(8) << 123 << '|' << std::endl <<
+        std::setw(8) << 1234567890 << '|' << std::endl;
 
 
 	return 0;
