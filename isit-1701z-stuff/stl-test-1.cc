7e236d2239cd8ad9c061e462d902a75f8883cb31
unyuu <u@nyuu.ru>
2020-05-19 10:51:31 +0500

    Сортировки пузырём больше с нами нет :( Теперь у нас вектор и std::sort

--- isit-1701z-stuff/stl-test-1.cc
+++ isit-1701z-stuff/stl-test-1.cc
@@ -6,52 +6,31 @@
  */
 
 #include <iostream>
-
-#include <cstdlib>
-
-int cmp_ints(const void * a, const void * b)
-{
-	int aa = *reinterpret_cast<const int *>(a);
-	int bb = *reinterpret_cast<const int *>(b);
-	if (aa > bb) return 1;
-	else if (bb > aa) return -1;
-	else return 0;
-}
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-	int * arr;
+	std::vector<int> arr;
 	unsigned qty;
 
 	std::cout << "Введите количество чисел: ";
 	std::cin >> qty;
 
-	arr = new int[qty];
-
-
+	arr.resize(qty);
 
 	for (unsigned i = 0; i < qty; i++) {
 		std::cout << "Введите элемент " << i << ": ";
 		std::cin >> arr[i];
 	}
 
-//	for (unsigned i = 0; i < qty-1; i++)
-//		for (unsigned j = i+1; j < qty; j++) {
-//			if (arr[j] < arr[i]) {
-//				auto tmp = arr[j];
-//				arr[j] = arr[i];
-//				arr[i] = tmp;
-//			}
-//		}
-	qsort(arr, qty, sizeof(int), cmp_ints);
+	std::sort(arr.begin(), arr.end());
 
 	std::cout << "Отсортированный массив:" << std::endl;
 	for (unsigned i = 0; i < qty; i++) {
 		std::cout << arr[i] << ' ';
 	}
 	std::cout << std::endl;
-
-	delete[] arr;
 
 	return 0;
 }
